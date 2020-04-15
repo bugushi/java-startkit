527382506b8b17221564c54aafb7aec3dac19e18
bugu <ginko@foxmail.com>
2020-04-15 10:39:50 +0800

    fix 跨模块autowired

--- api/src/main/java/com/example/demo/api/DemoApiApplication.java
+++ api/src/main/java/com/example/demo/api/DemoApiApplication.java
@@ -3,7 +3,7 @@ package com.example.demo.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.demo")
 public class DemoApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApiApplication.class, args);
